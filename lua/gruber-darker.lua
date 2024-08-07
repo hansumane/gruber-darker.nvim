1b9875e825b47476a25c480a41a62253c563aff2
Creasure <yurxo@bk.ru>
2024-08-08 01:53:32 +0300

    added more highlights

--- lua/gruber-darker.lua
+++ lua/gruber-darker.lua
@@ -3,7 +3,28 @@ local M = {}
 local function set_highlights()
   local palette = require("gruber-darker.palette")
   local highlights = {
-    CursorLineNr = { fg = palette.text, bold = true },
+    ColorColumn = { bg = palette.bg1 },
+    CursorLine = { bg = palette.bg1 },
+    Normal = { bg = palette.bg, fg = palette.fg },
+
+    LineNr = { fg = palette.bg4 },
+    CursorLineNr = { fg = palette.yellow, bold = true },
+
+    Delimiter = { fg = palette.fg },
+    Function = { fg = palette.niagara },
+    Identifier = { fg = palette.niagara },
+    String = { fg = palette.green },
+    Type = { fg = palette.quartz },
+    Typedef = { fg = palette.quartz },
+    Keyword = { fg = palette.yellow, bold = true },
+    Special = { fg = palette.yellow, bold = true },
+    SpecialChar = { fg = palette.yellow, bold = true },
+
+    IndentBlanklineChar = { fg = palette.bg1 },
+    IndentBlanklineSpaceChar = { fg = palette.bg1 },
+    IndentBlanklineSpaceCharBlankline = { fg = palette.bg1 },
+    IndentBlanklineContextChar = { fg = palette.bg2 },
+    IndentBlanklineContextSpaceChar = { fg = palette.bg2 },
   }
 
   for group, highlight in pairs(highlights) do
