9652f13b3b03c157acb810168b52ae654bed2c16
Creasure <yurxo@bk.ru>
2024-08-08 16:47:09 +0300

    added comments with original names for colors

--- lua/gruber-darker/palette.lua
+++ lua/gruber-darker/palette.lua
@@ -1,26 +1,26 @@
 local palette = {
   none      = "NONE",
-  fg        = "#e4e4ef",
-  fg1       = "#f4f4ff",
-  fg2       = "#f5f5f5",
-  white     = "#ffffff",
-  black     = "#000000",
-  bg_1      = "#101010",
-  bg        = "#181818",
-  bg1       = "#282828",
-  bg2       = "#453d41",
-  bg3       = "#484848",
-  bg4       = "#52494e",
-  red       = "#f43841",
-  red1      = "#ff4f58",
-  green     = "#73c936",
-  yellow    = "#ffdd33",
-  brown     = "#cc8c3c",
-  quartz    = "#95a99f",
-  niagara_2 = "#303540",
-  niagara_1 = "#565f73",
-  niagara   = "#96a6c8",
-  wisteria  = "#9e95c7",
+  fg        = "#e4e4ef", -- fg
+  fg1       = "#f4f4ff", -- fg+1
+  fg2       = "#f5f5f5", -- fg+2
+  white     = "#ffffff", -- white
+  black     = "#000000", -- black
+  bg_1      = "#101010", -- bg-1
+  bg        = "#181818", -- bg
+  bg1       = "#282828", -- bg+1
+  bg2       = "#453d41", -- bg+2
+  bg3       = "#484848", -- bg+3
+  bg4       = "#52494e", -- bg+4
+  red       = "#f43841", -- red
+  red1      = "#ff4f58", -- red+1
+  green     = "#73c936", -- green
+  yellow    = "#ffdd33", -- yellow
+  brown     = "#cc8c3c", -- brown
+  quartz    = "#95a99f", -- quartz
+  niagara_2 = "#303540", -- niagara-2
+  niagara_1 = "#565f73", -- niagara-1
+  niagara   = "#96a6c8", -- niagara
+  wisteria  = "#9e95c7", -- wisteria
 }
 
 return palette
