439c24cf28b689bcb102be47d152a8950022d7c5
Creasure <yurxo@bk.ru>
2025-06-22 00:04:43 +0300

    lualine: set different colors for a/b and y/z

--- lua/lualine/themes/gruber-darker.lua
+++ lua/lualine/themes/gruber-darker.lua
@@ -2,33 +2,11 @@ local c = require("gruber-darker.palette")
 
 return {
   normal = {
-    a = { bg = c.bg1, fg = c.fg, gui = "bold" },
-    b = { bg = c.bg1, fg = c.fg },
+    a = { bg = c.bg2, fg = c.fg2, gui = "bold" },
+    b = { bg = c.bg1, fg = c.fg1 },
     c = { bg = c.bg, fg = c.fg },
-  },
-  insert = {
-    a = { bg = c.bg2, fg = c.fg1, gui = "bold" },
-    b = { bg = c.bg2, fg = c.fg1 },
-    c = { bg = c.bg, fg = c.fg },
-  },
-  visual = {
-    a = { bg = c.yellow, fg = c.bg1, gui = "bold" },
-    b = { bg = c.bg2, fg = c.fg1 },
-    c = { bg = c.bg, fg = c.fg },
-  },
-  replace = {
-    a = { bg = c.bg2, fg = c.fg1, gui = "bold" },
-    b = { bg = c.bg2, fg = c.fg1 },
-    c = { bg = c.bg, fg = c.fg },
-  },
-  command = {
-    a = { bg = c.wisteria, fg = c.bg_1, gui = "bold" },
-    b = { bg = c.bg2, fg = c.fg1 },
-    c = { bg = c.bg, fg = c.fg },
-  },
-  inactive = {
-    a = { bg = c.bg, fg = c.fg, gui = "bold" },
-    b = { bg = c.bg, fg = c.fg },
-    c = { bg = c.bg, fg = c.fg },
+    x = { bg = c.bg, fg = c.fg },
+    y = { bg = c.bg1, fg = c.fg1 },
+    z = { bg = c.bg2, fg = c.fg2 }
   }
 }
