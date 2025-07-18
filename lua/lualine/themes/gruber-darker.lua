7ba9adb03f31d180228d4d6f083c7b0fdcffc0ac
Creasure <yurxo@bk.ru>
2025-07-18 23:46:31 +0300

    lualine: fix `bold`

--- lua/lualine/themes/gruber-darker.lua
+++ lua/lualine/themes/gruber-darker.lua
@@ -1,8 +1,10 @@
+local config = require('gruber-darker.config')
 local c = require('gruber-darker.palette')
+local styles = config.options.styles
 
 return {
   normal = {
-    a = { bg = c.bg2, fg = c.fg2, gui = 'bold' },
+    a = { bg = c.bg2, fg = c.fg2, bold = styles.bold },
     b = { bg = c.bg1, fg = c.fg1 },
     c = { bg = c.bg, fg = c.fg },
     x = { bg = c.bg, fg = c.fg },
